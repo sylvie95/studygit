e04b4eab82658591c7a42bc5b1cbb9adadfb8e83
sylvie95 <1451932421@qq.com>
2016-12-22 21:08:01 +0800

    Signed-off-by: sylvie95 <1451932421@qq.com>

--- ccs/src/main/java/com/yc/ccs/web/servlet/VcodeServlet.java
+++ ccs/src/main/java/com/yc/ccs/web/servlet/VcodeServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * 生成验证码的Servlet
+ * 生成验证码的Servlet   
  */
 @WebServlet("/vcode.jpg")
 public class VcodeServlet extends HttpServlet {
